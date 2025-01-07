f85b183429f110510326a48f2f3b5f80146d8851
dario martucci <dariomartucci@gmail.com>
2025-01-07 13:05:53 -0300

    commit para cambiar de rama a comentariosApi

--- assets/js/apiResenas.js
+++ assets/js/apiResenas.js
@@ -1,11 +1,28 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    const divPadreComentarios = document.querySelector('#card-comentarios');
+// Tomo el elemento padre.
+const divPadreComentarios = document.querySelector('#card-comentarios');
 
-    //URL de la API
-    const urlUsers = 'https://dummyjson.com/users?limit=15';
-    const urlComentarios = 'https://dummyjson.com/comments?limit=15';
+//URL de la API
+const urlUsers = 'https://dummyjson.com/users?limit=15';
+const urlComentarios = 'https://dummyjson.com/comments?limit=15';
 
-    function fetchURL()
+// función API
+async function fetchUrl(endpoint,objPadre) {
+    try {
+        const response = await fetch(endpoint);
+        if(!response.ok) {
+            throw new Error(`Error en la petición HTTP: ${response.status} -> ${response.statusText}`);
+        }
+        
+        const data = await response.json();
+        const usuario = data.user;
+        const comentarios = data.comments;
+
+        // Limpia el contenedor.
+        objPadre.innerHTML = '';
+
+        
+
+    }
 
 
-})
+}
