e6f738f9fc9c1ca3ef42f56e8cabe62e82597083
dario martucci <dariomartucci@gmail.com>
2025-01-07 19:36:04 -0300

    Se uso API random users para los usuarios y sus reseñas.Falta API comentarios en si.

--- assets/js/apiResenas.js
+++ assets/js/apiResenas.js
@@ -1,28 +1,59 @@
 // Tomo el elemento padre.
 const divPadreComentarios = document.querySelector('#card-comentarios');
+console.log(divPadreComentarios);
 
-//URL de la API
-const urlUsers = 'https://dummyjson.com/users?limit=15';
-const urlComentarios = 'https://dummyjson.com/comments?limit=15';
+if (!divPadreComentarios) {
+    console.error("El contenedor 'card-comentarios' no se encuentra en el DOM.");
+}
 
-// función API
-async function fetchUrl(endpoint,objPadre) {
+// URL de la API
+const urlUsers = 'https://randomuser.me/api/?results=10';
+console.log(urlUsers);
+
+// Función API
+async function fetchUsers(urlUsers) {
     try {
-        const response = await fetch(endpoint);
-        if(!response.ok) {
+console.log('Entrando al fetch...');
+
+        const response = await fetch(urlUsers);
+console.log(response);
+
+        if (!response.ok) {
             throw new Error(`Error en la petición HTTP: ${response.status} -> ${response.statusText}`);
         }
-        
+
         const data = await response.json();
-        const usuario = data.user;
-        const comentarios = data.comments;
+        const usuarios = data.results;
+console.log(usuarios);
 
         // Limpia el contenedor.
-        objPadre.innerHTML = '';
+        divPadreComentarios.innerHTML = '';
+console.log('Contenedor limpiado');
 
-        
+        // Crea un elemento para cada usuario.
+        usuarios.forEach(usuario => {
+            const divCardUsuario = document.createElement('div');
+console.log('Crea elemento div - divCardUsuario');
 
+            divCardUsuario.classList.add('card-comentario', 'flex-column');
+console.log(`Asigna clases al div: ${divCardUsuario.classList.length}`);
+
+            // Construye el HTML para cada usuario.
+            divCardUsuario.innerHTML = `
+                <img class="card-comentario-img img-user" src="${usuario.picture.large}" alt="${usuario.name.first}">
+                <h3 class="card-coment-nombre centrado-lineal">${usuario.name.first} ${usuario.name.last}</h3>
+            `;
+console.log(`divCardUsuario: ${divCardUsuario}`);
+
+            divPadreComentarios.appendChild(divCardUsuario);
+        });
+    } catch (error) {
+        console.error('Error de conexión:', error.message);
     }
-
-
 }
+
+// Espera a que el DOM esté completamente cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    fetchUsers(urlUsers);
+});
+
